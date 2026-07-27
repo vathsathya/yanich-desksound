cb9d79e697c35d85c85e5420e6e5edd452f5b2b5
Vath Sathya <vath.sathya@gmail.com>
2026-07-28 04:51:11 +0700

    Bump version to v1.2.3 and release updated multi-platform GUI

--- server-windows/include/version.h
+++ server-windows/include/version.h
@@ -3,10 +3,10 @@
 
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 2
-#define APP_VERSION_PATCH 2
+#define APP_VERSION_PATCH 3
 #define APP_VERSION_BUILD 0
 
-#define APP_VERSION_STRING "1.2.2"
-#define APP_VERSION_TAG "v1.2.2"
+#define APP_VERSION_STRING "1.2.3"
+#define APP_VERSION_TAG "v1.2.3"
 
 #endif // VERSION_H
