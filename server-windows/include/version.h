d97dde452820da26c117177c29e400afe1e75e6a
Vath Sathya <vath.sathya@gmail.com>
2026-07-28 05:06:38 +0700

    Include gradle-wrapper.jar for Android build on CI and bump version to v1.2.5

--- server-windows/include/version.h
+++ server-windows/include/version.h
@@ -3,10 +3,10 @@
 
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 2
-#define APP_VERSION_PATCH 4
+#define APP_VERSION_PATCH 5
 #define APP_VERSION_BUILD 0
 
-#define APP_VERSION_STRING "1.2.4"
-#define APP_VERSION_TAG "v1.2.4"
+#define APP_VERSION_STRING "1.2.5"
+#define APP_VERSION_TAG "v1.2.5"
 
 #endif // VERSION_H
